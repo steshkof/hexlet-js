8a916e21a98acfd728bfd23407b3a5e2cac477ae
steshkof <steshkof@gmail.com>
2022-05-06 09:48:06 +0300

    file structure correction

--- hexlet-hello-world/index.js
+++
@@ -1,5 +0,0 @@
-import _ from "lodash";
-
-console.log("Hello, Hexlet!");
-Math.sqrt(256) + 100;
-console.log(_.last(['one', 'two']))
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+import _ from "lodash";
+
 console.log("Hello, Hexlet!");
-Math.sqrt(256) + 100
+Math.sqrt(256) + 100;
+console.log(_.last(['one', 'two']))
