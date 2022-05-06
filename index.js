95f4c6b10aea203debca9a4904ed8ff33b4d95c6
steshkof <steshkof@gmail.com>
2022-05-06 09:54:55 +0300

    change index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import _ from "lodash";
 
 console.log("Hello, Hexlet!");
 Math.sqrt(256) + 100;
-console.log(_.last(['one', 'two']))
+console.log(_.last(["one", "two"]));
+
+const find_sum = (a, b) => {
+  const c = a + b;
+  return c;
+};
